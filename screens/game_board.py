eb2b9b1215c3bb024cc5a4c5a91ee026c7a387f7
Matheus da Silva Rego <matheus.gomes@al.infnet.edu.br>
2021-08-16 15:56:47 -0500

    Criando tela de criação de jogadores

--- screens/game_board.py
+++ screens/game_board.py
@@ -1,19 +1,16 @@
-lines = 10
-columns = 10
-
-space = ' '
-empty = ''
-
 class GameBoard():
+    
+    def print_game_board(players):
+        columns = 10
+        lines = 10
 
-    def print_game_board():
-        print( f'\n{space * 3}', end=empty )
-        for position_column in range( 0, columns ):
-            number_of_column = str( position_column )
-            print( number_of_column + (space * 2), end=empty )
+        space = ' '
+        empty = ''
         
-        print( '' )
+        print( '\n     ', end=empty )
+        for i in range( 0, columns ):
+            print( str( i ) + '  ', end=empty )
 
-        for position_line in range( 0, lines ): 
-            number_of_line = str( position_line ) + ':'
-            print( number_of_line, space + '*  ' * columns )
+        print( '' )
+        for i in range( 0, lines ):
+            print( str( i ) + ':', '  *' * columns, ' |' )
