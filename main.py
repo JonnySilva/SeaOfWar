9f63cf9ec1901abfa7f5c3c7584c9107409d61c7
Matheus da Silva Rego <matheus.gomes@al.infnet.edu.br>
2021-08-16 15:57:16 -0500

    Adicionando tela de criação dejogadores a main

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from screens import game_board as gameboard
+from screens import players
+
 def main():
-    gameboard.GameBoard.print_game_board()
+    playersList = players.Players.screen()
+    gameboard.GameBoard.print_game_board( playersList )
 
 if( __name__ == "__main__" ):
     main()
