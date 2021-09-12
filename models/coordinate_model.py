493985fd200aa213832913f39687606152e12d11
Matheus da Silva Rego <matheus.gomes@al.infnet.edu.br>
2021-09-12 17:18:35 -0300

    Criando método para resetar objeto das coordenadas

--- models/coordinate_model.py
+++ models/coordinate_model.py
@@ -3,9 +3,15 @@ class CoordinateModel( object ):
     def __init__( self, coordinate_x = -1, coordinate_y = -1, position = "" ):
         super().__init__()
         
+        self.reset()
         self._coordinate_x = coordinate_x
         self._coordinate_y = coordinate_y
         self._position = position
+        
+    def reset( self ):
+        self._coordinate_x = -1
+        self._coordinate_y = -1
+        self._position = ""
         
     @property
     def coordinate_x( self ):
