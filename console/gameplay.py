1abc53c7900f0c0d713314317064ba5fe8f3079c
Matheus da Silva Rego <matheus.gomes@al.infnet.edu.br>
2021-09-12 12:56:14 -0300

    Aplicando objeto de coordenadas ao código

--- console/gameplay.py
+++ console/gameplay.py
@@ -2,6 +2,7 @@ from utils.utils import Utils as UTILS
 from utils.MESSAGES import Messages as MESSAGES
 import utils.CONSTANTS as CONSTANTS
 from console.game_board import GameBoard as GameBoard
+from models.coordinate_model import CoordinateModel
 
 class GamePlay:
     
@@ -35,67 +36,63 @@ class GamePlay:
             
         return False
     
-    def position_ship( grid, ship_size, coordinate_y, coordinate_x, isvertical=False, verbose=True ):
-        if isvertical:
-            if not GamePlay.valid_coordinates( grid, coordinate_y, ( coordinate_y + ship_size - 1 ), coordinate_x, coordinate_x, verbose ):
+    def position_ship( grid, ship_size, coordinate_model, verbose=True ):
+        if coordinate_model.position == "V":
+            if not GamePlay.valid_coordinates( grid, coordinate_model.coordinate_y, ( coordinate_model.coordinate_y + ship_size - 1 ), coordinate_model.coordinate_x, coordinate_model.coordinate_x, verbose ):
                 # print( f"\t\t ADD: {ship_size}" )
                 for i in range( ship_size ):
-                    grid[coordinate_y + i][coordinate_x] = ship_size
+                    grid[coordinate_model.coordinate_y + i][coordinate_model.coordinate_x] = ship_size
                 return True
             else:
                 return False
-        else:
-            if not GamePlay.valid_coordinates( grid, coordinate_y, coordinate_y, coordinate_x, ( coordinate_x + ship_size - 1 ), verbose ):
+        elif coordinate_model.position == "H":
+            if not GamePlay.valid_coordinates( grid, coordinate_model.coordinate_y, coordinate_model.coordinate_y, coordinate_model.coordinate_x, ( coordinate_model.coordinate_x + ship_size - 1 ), verbose ):
                 for i in range( ship_size ):
-                    grid[coordinate_y][coordinate_x + i] = ship_size
+                    grid[coordinate_model.coordinate_y][coordinate_model.coordinate_x + i] = ship_size
                 return True
             else:
                 return False
     
-    def insert_coordinate( ship_model, coordinate_x, coordinate_y ):
-        while coordinate_x < 0 or coordinate_y < 0 or coordinate_x > CONSTANTS.SIZE or coordinate_y > CONSTANTS.SIZE:
+    def insert_coordinate( ship_model, coordinate_model ):
+        while coordinate_model.coordinate_x < 0 or coordinate_model.coordinate_y < 0 or coordinate_model.coordinate_x > CONSTANTS.SIZE or coordinate_model.coordinate_y > CONSTANTS.SIZE:
             ship_name = UTILS.ship_names_enum(ship_model.ship_name)
             
             print( f"\nDigite a coordenada que deseja colocar o {ship_name} ({ship_model.ship_size} casas): \n(exemplo: A1)\n" )
             coordinate = input( "> " )
             
             try:
-                coordinate_x = int( UTILS.letter_to_column_number( coordinate[0].upper() ) )
+                coordinate_model.coordinate_x = int( UTILS.letter_to_column_number( coordinate[0].upper() ) )
             except:
                 break
             else:
                 try:
-                    coordinate_y = int( coordinate[1] )
+                    coordinate_model.coordinate_y = int( coordinate[1] )
                 except:
                     break
                     
-            return coordinate_x, coordinate_y
+            return coordinate_model
     
     def vertical_or_horizontal():
-        is_vertical = MESSAGES.QUESTION_HORIZONTAL_OR_VERTICAL().upper()
+        position = MESSAGES.QUESTION_HORIZONTAL_OR_VERTICAL().upper()
         
-        while ( is_vertical != "V" ) and ( is_vertical != "H" ):
-            is_vertical = MESSAGES.QUESTION_WARNING_POSITION().upper()
+        while ( position != "V" ) and ( position != "H" ):
+            position = MESSAGES.QUESTION_WARNING_POSITION().upper()
         
-        is_vertical = is_vertical == "V"
-        
-        return is_vertical
+        return position
     
     def insert_ship():
         for ship_model in CONSTANTS.LIST_OF_SHIP_MODELS:
-            placed = False
-            coordinate_x = -1
-            coordinate_y = -1
-            is_vertical = ""
+            inserted = False
+            coordinate_model = CoordinateModel()
             
-            while not placed:
-                coordinate_x, coordinate_y = GamePlay.insert_coordinate( ship_model, coordinate_x, coordinate_y )                
+            while not inserted:
+                coordinate_model = GamePlay.insert_coordinate( ship_model, coordinate_model )                
                 
-                is_vertical = GamePlay.vertical_or_horizontal()
+                coordinate_model.position = GamePlay.vertical_or_horizontal()
                 
                 temporary_grid = CONSTANTS.GRID_GAME_BOARD
                 
-                placed = GamePlay.position_ship( temporary_grid, ship_model.ship_size, coordinate_y, coordinate_x, is_vertical )
+                inserted = GamePlay.position_ship( temporary_grid, ship_model.ship_size, coordinate_model )
                 
                 UTILS.clear()
                 GameBoard.draw_game_board( temporary_grid )
